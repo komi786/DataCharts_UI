0d8adca7a9d26a66704820fb913608d08538d7c7
Komal Gilani <komalsyeda29@gmail.com>
2023-05-03 15:32:46 +0500

    Initial Commit

---
+++ DataCharts_UI/DataCharts_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataCharts_UIApp.swift
+//  DataCharts_UI
+//
+//  Created by Komal Gilani on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DataCharts_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
